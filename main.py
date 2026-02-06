4594e26c3f9b4e7d6dbe540cf0dab3e0cb8038d7
gabriel <gabrielsbadenas@gmail.com>
2026-02-05 23:07:51 -0300

    cuentas

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 output = 'python program'
 requirements = "none"
+money = {}
+money['lemoncash'] = 152426.84
+money['gym'] = 94000
+money['left'] = money['lemoncash'] - money['gym'] 
+output = money
 print(output)
