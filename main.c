1c69d3ccd881733417c14414a85ce0dd5a4bed78
gabrielf.sanchez@bue.edu.ar <gabrielf.sanchez@bue.edu.ar>
2026-03-03 12:21:37 +0000

    c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int fahr, celsius, lower, upper, step;
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%3d %6d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    printf("Hello, World!");
+    return 0;
+}
